67cd7c055c35f6ba11428b620d3fbe1d3015c7d2
Fúlvio Leo <fulviokaldas@outlook.com>
2024-05-25 16:59:05 -0300

    initial commit with calculator ready to use

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import "./Button.css";
+
+export default (props) => (
+  <button
+    onClick={() => props.click(props.label)}
+    className={`
+    button
+    ${props.operation ? "operation" : ""}
+    ${props.double ? "double" : ""}
+    ${props.triple ? "triple" : ""}
+  `}
+  >
+    {props.label}
+  </button>
+);
